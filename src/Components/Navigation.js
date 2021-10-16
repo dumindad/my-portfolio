0cebb7e84bef15958ef8f7024db139095fda7014
duminda <dumindad@gmail.com>
2021-10-17 02:25:57 +0600

    footer commit

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -25,7 +25,7 @@ function Navigation({navToggle, setNavToggle}) {
                 </li>
             </ul>
             <footer className="footer">
-                <p>@2021 <b>Lorem Ipsum</b></p>
+                <p>@2021 <b>De Silva</b></p>
             </footer>
         </NavigationStyled>
     )
