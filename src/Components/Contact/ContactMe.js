dac9900bc004290ea132e6343f6f12bf6244830b
dumindad <88272815+dumindad@users.noreply.github.com>
2021-10-25 14:15:20 +0600

    Update ContactMe.js

--- src/Components/Contact/ContactMe.js
+++ src/Components/Contact/ContactMe.js
@@ -52,12 +52,12 @@ const ContactMe = () => {
         // Use emailjs to email contact form data
         await emailjs.send(
           //process.env.REACT_APP_SERVICE_ID,
-            process.secret.REACT_APP_SERVICE_ID,
+            process.secrets.REACT_APP_SERVICE_ID,
           //process.env.REACT_APP_TEMPLATE_ID,
-            process.secret.REACT_APP_TEMPLATE_ID,
+            process.secrets.REACT_APP_TEMPLATE_ID,
           templateParams,
           //process.env.REACT_APP_USER_ID
-            process.secret.REACT_APP_USER_ID
+            process.secrets.REACT_APP_USER_ID
         );
   
         // Reset contact form fields after submission
